bd223e797df2323a236c229c898da0ac801e7132
DeM4rcio <henriquemarcio170@gmail.com>
2024-12-23 17:27:14 -0300

    função para encontrar mediana

--- 4. Median of Two Sorted Arrays/main.c
+++ 4. Median of Two Sorted Arrays/main.c
@@ -37,3 +37,16 @@ int findKth(int* nums1, int size1, int* nums2, int size2, int k) {
         return findKth(nums1, size1, nums2 + idx2, size2 - idx2, k - idx2);
     }
 }
+
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int totalSize = nums1Size + nums2Size;
+
+    if (totalSize % 2 == 1) {
+        return findKth(nums1, nums1Size, nums2, nums2Size, (totalSize / 2) + 1);
+    } else {
+        int left = findKth(nums1, nums1Size, nums2, nums2Size, totalSize / 2);
+        int right = findKth(nums1, nums1Size, nums2, nums2Size, (totalSize / 2) + 1);
+        return (left + right) / 2.0;
+    }
+}
